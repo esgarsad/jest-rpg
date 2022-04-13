2a00bf4d61b4eaa1fa3dcceab1369b618d5a96e1
esgar <navaedgar36@yahoo.com.mx>
2022-04-13 18:58:18 -0500

    players

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,11 +1,11 @@
 const Potion = require('../lib/Potion.js');
-// test('creates a random potion object', () => {
-//   const potion = new Potion();
+ test('creates a random potion object', () => {
+   const potion = new Potion();
 
-//   expect(potion.name).toEqual(expect.any(String));
-//   expect(potion.name.length).toBeGreaterThan(0);
-//   expect(potion.value).toEqual(expect.any(Number));
-// });
+   expect(potion.name).toEqual(expect.any(String));
+   expect(potion.name.length).toBeGreaterThan(0);
+   expect(potion.value).toEqual(expect.any(Number));
+ });
 test('creates a random potion object', () => {
     const potion = new Potion();
   
